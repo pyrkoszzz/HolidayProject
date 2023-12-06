c4aecefe0236ccc913afd3f1a3ab7e363312ef7c
pyrkoszzz <pepexd555@gmail.com>
2023-12-06 10:36:17 +0100

    Create action in the controller ViewPropertyDetails, Define a view model PropertyDetailsModel

--- WebApplication1/Controllers/PropertyListingController.cs
+++ WebApplication1/Controllers/PropertyListingController.cs
@@ -5,7 +5,7 @@ namespace WebApp.Controllers
 {
     public class PropertyListingController : Controller
     {
-        private static List<PropertyListingModel> properties = new()
+        private static List<PropertyDetailsModel> properties = new()
         {
             new()
             {
@@ -14,7 +14,10 @@ namespace WebApp.Controllers
                 Blurb = "Beautiful cottage on the Cornwall coast",
                 Location = "Cornwall",
                 NumberOfBedrooms = 3,
-                CostPerNight = 350
+                CostPerNight = 350,
+                Description = "A beautifully converted 1945 boat nestled within a private forest overlooking the beautiful open fenland countryside. Perfect retreat for couples looking to relax, explore and visit the local towns. Located 15 minutes drive from Ely and 30 minutes from Cambridge.",
+                Amenities = new List<string> { "Garden view", "Wifi", "Dedicated workspace" },
+                BookedDates = new List<DateTime> { new DateTime(2023, 12, 6), new DateTime(2023, 12, 7), new DateTime(2023, 12, 8) }
             },
             new()
             {
@@ -23,17 +26,18 @@ namespace WebApp.Controllers
                 Blurb = "Stately home on the Devon moores",
                 Location = "Devon",
                 NumberOfBedrooms = 7,
-                CostPerNight = 730
+                CostPerNight = 730,
+                Description = "We welcome you to come and stay in one of our newly renovated Airbnb rooms set within our beautiful converted barn in the Whicham Valley. A peaceful, picturesque location with rural open views of the Combes. With Silecroft beach just a 5 minute drive away and Black Combe Fell (one of the Lake District's famous Wainwright's) walking distance from the barn.",
+                Amenities = new List<string> { "Hair dryer", "Wifi", "Backyard" },
+                BookedDates = new List<DateTime> { new DateTime(2023, 12, 9), new DateTime(2023, 12, 10), new DateTime(2023, 12, 11) }
             }
         };
 
-        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
         public IActionResult ListAll()
         {
             return View("ListProperties", properties);
@@ -42,7 +46,13 @@ namespace WebApp.Controllers
         public ActionResult ListAvailable(DateTime start, DateTime end)
         {
             // Filter properties based on availability between start and end dates
-            return View("ListAll");
+            return View("ListProperties", properties);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult ViewPropertyDetails(int id)
+        {
+            return View("ListProperties", properties);
         }
     }
 }
---
+++ WebApplication1/Models/PropertyDetailsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models
+{
+    public class PropertyDetailsModel : PropertyListingModel
+    {
+        public string Description { get; set; }
+        public List<string> Amenities { get; set; }
+        public List<DateTime> BookedDates { get; set; }
+    }
+}
