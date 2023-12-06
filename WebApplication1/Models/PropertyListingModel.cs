8889c4eb350cff185d396db5fb3997b32a5f55a5
pyrkoszzz <pepexd555@gmail.com>
2023-12-06 10:16:30 +0100

    Define a view model class PropertyListingModel

---
+++ WebApplication1/Models/PropertyListingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class PropertyListingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Blurb { get; set; }
+        public string Location { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public float CostPerNight { get; set; }
+    }
+}
