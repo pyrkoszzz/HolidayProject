7605724ad6133c2f5e577326e7b537f0288b9767
pyrkoszzz <pepexd555@gmail.com>
2023-12-06 10:15:59 +0100

    Add MVC controller PropertyListingController

---
+++ WebApplication1/Controllers/PropertyListingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PropertyListingController : Controller
+    {
+        private static List<PropertyListingModel> properties = new()
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Rose Cottage",
+                Blurb = "Beautiful cottage on the Cornwall coast",
+                Location = "Cornwall",
+                NumberOfBedrooms = 3,
+                CostPerNight = 350
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Safron House",
+                Blurb = "Stately home on the Devon moores",
+                Location = "Devon",
+                NumberOfBedrooms = 7,
+                CostPerNight = 730
+            }
+        };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ListAll()
+        {
+            return View();
+        }
+
+        public ActionResult ListAvailable(DateTime start, DateTime end)
+        {
+            // Filter properties based on availability between start and end dates
+            return View("ListAll");
+        }
+    }
+}
