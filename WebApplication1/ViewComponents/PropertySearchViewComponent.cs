d45d0341a0baf5a68f258fa1f64233ea5f7dcb66
pyrkoszzz <pepexd555@gmail.com>
2023-12-06 12:56:34 +0100

    Create a View Component which will act as a top level menu and allows the user to search for available properties.

---
+++ WebApplication1/ViewComponents/PropertySearchViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewComponents
+{
+    public class PropertySearchViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
