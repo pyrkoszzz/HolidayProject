16a143955c7392719f1f8af02c7abe99b91d239c
pyrkoszzz <pepexd555@gmail.com>
2023-12-07 10:19:54 +0100

    Create controller PropertyManagementController. Implement a pair of actions which allow a new property to be registered, create a form which captures the relevant details and instanciates a PropertyDetailsModel.

---
+++ WebApplication1/Controllers/PropertyManagementController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PropertyManagementController : Controller
+    {
+        public IActionResult RegisterProperty()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterProperty(PropertyDetailsModel propertyDetailsModel)
+        {
+            return View(propertyDetailsModel);
+        }
+    }
+}
